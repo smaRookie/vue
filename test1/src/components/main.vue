f6aa579b85366070b1da6ae1efc47c8a9f254cf7
dl <874491267@qq.com>
2017-09-18 21:11:10 +0800

    11

---
+++ test1/src/components/main.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="main">
+    <div  ref="child" id="fff">
+      dddddddd
+    </div>
+  </section>
+</template>
+<script>
+  export default {
+    name: 'main',
+    methods: {
+      el () {
+      },
+      re () {
+        let f = this.$refs.child.innerHTML
+        let h = document.getElementById('fff').getBoundingClientRect()
+        console.log('h', h)
+        console.log(f)
+      }
+    },
+    mounted () {
+      this.el()
+      this.re()
+    }
+  }
+</script>
+<style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+  div {
+    position: absolute;
+    top: 10px;
+    left: 20px;
+  }
+</style>
